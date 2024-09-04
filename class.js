f9e582cb0b2ae72f98b0667a1e888e2bce83ee30
ighogens <ighogens@gmail.com>
2024-09-04 22:40:54 +0100

    first commit

---
+++ class.js
@@ -0,0 +1,17 @@
+// const, let, var
+
+// +, -, *, 
+
+
+let currentAge = 5;
+
+let newAge = currentAge + 10;
+
+console.log(newAge)
+
+const myName = "Emmanuel"
+
+console.log("Hello my name is" + myName)
+
+
+// if statement  = is an assignment while == is a conparator
